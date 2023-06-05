17990868fd2d46df4bfc65f014e5369aa43ecf1c
HazmiHazim <hazimnetto01@gmail.com>
2023-06-05 18:21:28 +0800

    Change DB name

--- app/src/main/java/com/iot/smart_lighting/Model/SmartLampDB.java
+++ app/src/main/java/com/iot/smart_lighting/Model/SmartLampDB.java
@@ -4,11 +4,11 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class SmartLedDB extends SQLiteOpenHelper {
+public class SmartLampDB extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "SmartLED.db";
+    public static final String DATABASE_NAME = "SmartLamp.db";
 
-    public SmartLedDB(Context context) {
+    public SmartLampDB(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
